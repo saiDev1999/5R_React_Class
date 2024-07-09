ada4e604909173c5a8d5b609c394208fc215e60d
sai worms <sai@wormsconnect.com>
2024-07-09 16:00:47 +0530

    state mangement in react

--- src/screens/home-screen.js
+++ src/screens/home-screen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Navbar from "../components/navBar/navbar";
 import ButtonComponent from "../components/button/button";
 import { Link } from "react-router-dom";
@@ -6,13 +6,22 @@ import axios from "axios";
 import UseEffectExample from "../components/useEffect/useEffectEx1";
 import UseEffectExample2 from "../components/useEffect/useEffectEx2";
 import UseEffectEx3 from "../components/useEffect/useEffectEx3";
+import { ProfileWrapper } from "../navigation/stack";
 
 function HomeScreen() {
+  const sharedData = useContext(ProfileWrapper);
+  console.log(sharedData, "sharedData");
   return (
     <div>
       <Navbar />
-      <h2>welcome to home screen</h2>
-      <UseEffectEx3 />
+      <h2>
+        welcome to home screen {sharedData.profileInfo.name}{" "}
+        {sharedData.profileInfo.salary}
+      </h2>
+
+      <h4>{sharedData.counter}</h4>
+
+      {/* <UseEffectEx3 /> */}
       {/* <UseEffectExample2 />
       <UseEffectExample /> */}
     </div>
